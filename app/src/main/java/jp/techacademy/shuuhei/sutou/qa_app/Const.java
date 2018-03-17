7b6b1a2f4d48ba38f8597aa446d7b2bc7ba60fb5
siro178 <sutou66shuhei@gmail.com>
2018-03-17 19:05:00 +0900

    エラーが解決できません。

---
+++ app/src/main/java/jp/techacademy/shuuhei/sutou/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.shuuhei.sutou.qa_app;
+
+/**
+ * Created by user on 2018/03/16.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users";
+    public static final String ContentsPATH = "contents";
+    public static final String AnswersPATH = "answers";
+
+    public static final String NameKEY = "name";
+}
