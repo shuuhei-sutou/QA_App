9bcc68fd5cf91c5965c08cf1a9bd0de010048cff
siro178 <sutou66shuhei@gmail.com>
2018-03-22 15:13:00 +0900

    int mGenre = extras.getInt("genre");　でmGenreに値が格納されないです。

--- app/src/main/java/jp/techacademy/shuuhei/sutou/qa_app/QuestionDetailActivity.java
+++ app/src/main/java/jp/techacademy/shuuhei/sutou/qa_app/QuestionDetailActivity.java
@@ -9,8 +9,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Base64;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,6 +27,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.io.ByteArrayOutputStream;
 
 public class QuestionDetailActivity extends AppCompatActivity {
 
@@ -30,6 +36,8 @@ public class QuestionDetailActivity extends AppCompatActivity {
     private QuestionDetailListAdapter mAdapter;
 
     private DatabaseReference mAnswerRef;
+    int count = 0;
+    private int mGenre;
 
     private ChildEventListener mEventListener = new ChildEventListener() {
         @Override
@@ -80,25 +88,66 @@ public class QuestionDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_question_detail);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final Button button = (Button)findViewById(R.id.likeButton);
+        Button button = (Button)findViewById(R.id.likeButton);
+
         if (user == null) {
             button.setVisibility(View.GONE);
 
-        } else {
+        }
+ /*       else if((user != null) && ((count%2) == 0)) {
+            button.setText("登録済み");
+            count += 1;
+
+        }else if((user != null) && ((count%2) != 0)){ */
+ else{
+            String title = button.getText().toString();
+            Log.d("title", "" + title + "");
             button.setVisibility(View.VISIBLE);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    button.setText("登録済み");
 
-                    TextView mTitleTextView = (TextView) findViewById(R.id.titleTextView);
+                    // 渡ってきたジャンルの番号を保持する
+                    Bundle extras = getIntent().getExtras();
+                    int mGenre = extras.getInt("genre");
+                    TextView mBodyText = (TextView) findViewById(R.id.bodyTextView);
+                    ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+                    mImageView.setOnClickListener(this);
 
-                    String title = mTitleTextView.getText().toString();
+                    DatabaseReference dataBaseReference = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference genreRef = dataBaseReference.child(Const.ContentsPATH).child(String.valueOf(mGenre));
 
                     Map<String, String> data = new HashMap<String, String>();
-                    data.put("contents", title);
 
-                    Log.d("qqqqq", "QQQQQ");
+                    // UID
+                    data.put("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                    // 本文を取得する
+                    String body = mBodyText.getText().toString();
+
+                    // Preferenceから名前を取る
+                    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    String name = sp.getString(Const.NameKEY, "");
+
+                    data.put("body", body);
+                    data.put("name", name);
+
+                    // 添付画像を取得する
+                    BitmapDrawable drawable = (BitmapDrawable) mImageView.getDrawable();
+
+                    // 添付画像が設定されていれば画像を取り出してBASE64エンコードする
+                    if (drawable != null) {
+                        Bitmap bitmap = drawable.getBitmap();
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, baos);
+                        String bitmapString = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+                        data.put("image", bitmapString);
+                    }
+
+                    genreRef.push().setValue(data, this);
+
+
                 }
             });
         }
